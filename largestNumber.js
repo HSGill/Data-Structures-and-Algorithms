0ecf2feb7a86bb2e2bbb1a42249463c535891c12
HSGill <harjotgill73@gmail.com>
2018-11-16 21:55:37 +1300

    fixed negative value issue

--- largestNumber.js
+++ largestNumber.js
@@ -8,7 +8,7 @@ function largestOfFour(arr) {
     var A = [];
 
     for (var i = 0; i < arr.length; i++) {
-        var largestNumber = 0;
+        var largestNumber = arr[i][0];
         for (var j = 1; j < arr[i].length; j++) {
             if (arr[i][j] > largestNumber) {
                 largestNumber = arr[i][j];
@@ -18,6 +18,6 @@ function largestOfFour(arr) {
     }
     return A;
 }
- console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+ console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]));
 
 
