9743509a401cf726144b2e5c4844810aa4d0c28a
HSGill <harjotgill73@gmail.com>
2018-11-28 05:01:42 +1300

    changes

--- twoSum.js
+++ twoSum.js
@@ -10,18 +10,18 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1]. */
 
 var twoSum = function (nums, target) {
-   for (let i=0;i<nums.length-1;i++){
-       for(let j=i+1; j<nums.length;j++){
-        if(nums[i]+nums[j]===target){
-            console.log(i+ ',' + j);
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                console.log(i + ',' + j);
+            }
+            console.log('no match', i)
         }
-       console.log('no match',i)
-       }
-   }
-   return false; 
+    }
+    return false;
 
 };
 
-nums = [21,  7, 5,6,3,4,11, 15];
-target= 9;
-console.log(twoSum(nums,target));
+nums = [21, 7, 5, 6, 3, 4, 11, 15];
+target = 9;
+console.log(twoSum(nums, target));
