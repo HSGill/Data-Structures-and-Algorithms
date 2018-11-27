08005b32f8df5571bda2b13834683cde3f2713c2
HSGill <harjotgill73@gmail.com>
2018-11-28 04:23:05 +1300

    changes

--- twoSum.js
+++ twoSum.js
@@ -13,14 +13,13 @@ var twoSum = function (nums, target) {
    for (let i=0;i<nums.length-1;i++){
        for(let j=i+1; j<nums.length;j++){
         if(nums[i]+nums[j]===target){
-            return(i+ ',' + j);
+            console.log(i+ ',' + j);
         }
-       console.log('no match')
+       console.log('no match',i)
        }
-      
-   } 
-   
-    
+   }
+   return false; 
+
 };
 
 nums = [21,  7, 5,6,3,4,11, 15];
