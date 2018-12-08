2bbbd2a14783f08d9118c8f897aef8d62fda1dd2
HSGill <harjotgill73@gmail.com>
2018-12-09 11:54:33 +1300

    fibonacci using recursion

---
+++ fibonacci.js
@@ -0,0 +1,23 @@
+// Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+
+//For example: fibonacciRecursive(6) should return 8
+
+function fibonacciIterative(n){
+    //code here;
+    let array = [0,1]
+    for(let i=0;i<n;i++){
+        array.push(array[i]+array[i-1]);
+    }
+    return array[i];
+
+  }
+  fibonacciIterative(3);
+  
+  function fibonacciRecursive(n) {
+    //code here;
+  }
+  
+  fibonacciRecursive(3)
