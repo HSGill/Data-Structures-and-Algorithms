d3b4808a7bd9f1af673612e9e147e45744b9164d
HSGill <harjotgill73@gmail.com>
2019-02-26 19:42:53 +1300

    find Number in an array

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-let sum = (num1,num2) =>{
+/* let sum = (num1,num2) =>{
     let sum = num1+num2;
     if(sum>20){
         return console.log('Hi');
@@ -7,4 +7,15 @@ let sum = (num1,num2) =>{
     else {return console.log("nothing")}
 }
 
-sum(10, 5);
+sum(10, 5); */
+
+let findNumber = (array, number) => {
+  for (i=0;i<array.length;i++){
+      if(array[i]===number){
+         return 'YES';
+      }
+  }
+  return 'NO';
+}
+
+findNumber([1,2,3,4,5], 5);
